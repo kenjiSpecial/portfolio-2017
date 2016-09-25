8995cc24884260e62594d03423ec761f2231db46
Kenji Saito <k.saito.1985@gmail.com>
2016-09-25 15:39:17 -0700

    add stats.js and add keydown event.

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,10 @@
 const TweenMax = require('gsap');
 const glslify = require('glslify');
 const THREE = require('three');
+const Stats = require('stats.js');
 
-var camera, scene, renderer, mouse;
-var geometry, shaderMaterial, mesh;
+var camera, scene, renderer, mouse, stats, geometry, shaderMaterial, mesh, clock;
+var isLoop;
 
 function init() {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
@@ -25,27 +26,36 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0x000000);
 
+    stats = new Stats();
+    clock = new THREE.Clock();
+    document.body.appendChild( stats.dom );
+
     document.body.appendChild(renderer.domElement);
     document.addEventListener('mousemove', onDocumentMouseMove, false);
 }
 
 function loop() {
-    requestAnimationFrame(loop);
+    var delta = clock.getDelta();
+    console.log(delta);
 
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
 
+
     renderer.render(scene, camera);
+    stats.update()
 }
 
 require('domready')(() => {
     init();
-
+    isLoop = true;
     TweenMax.ticker.addEventListener("tick", loop);
 });
 
 function onDocumentMouseMove(event){
     event.preventDefault();
+    if(!mouse) mouse = new THREE.Vector2();
+
     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     mouse.y = -( event.clientY / window.innerHeight ) * 2 + 1;
 }
@@ -55,4 +65,19 @@ window.addEventListener("resize", function(ev){
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+window.addEventListener('keydown', function(ev){
+    switch(ev.which){
+        case 27:
+            isLoop = !isLoop;
+            if(isLoop) {
+                clock.stop();
+                TweenMax.ticker.addEventListener("tick", loop);
+            }else{
+                clock.start();
+                TweenMax.ticker.removeEventListener("tick", loop);
+            }
+            break;
+    }
 });
